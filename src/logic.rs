04add813fbb922635a1c0b7afa3e0c2f683e7c6b
HoolaBoola <ale@hoolaboola.fi>
2022-01-25 22:24:03 +0200

    Buncha tests

--- src/logic.rs
+++ src/logic.rs
@@ -1,5 +1,5 @@
 pub fn calculate_infix(input: &str) -> String {
-    shunting_yard(input)
+    shunting_yard(input).unwrap()
         .into_iter()
         .map(|x| format!("{x:?}"))
         .collect::<Vec<_>>()
@@ -17,7 +17,7 @@ fn precedence(c: char) -> Option<u8> {
 
 use Token::*;
 
-fn shunting_yard(input: &str) -> Vec<Token> {
+fn shunting_yard(input: &str) -> Result<Vec<Token>, &str> {
     let mut output = Vec::new();
     let mut operators = Vec::new();
 
@@ -63,16 +63,30 @@ fn shunting_yard(input: &str) -> Vec<Token> {
             }
 
             if !found {
-                panic!("Matching left parenthesis not found");
+                return Err("Right parenthesis without a pair found");
             }
         }
     }
     while let Some(op) = operators.pop() {
+        if op == '(' {
+            return Err("Left parenthesis without a pair found");
+        }
         output.push(Operator(op));
     }
-    output
+    Ok(output)
 }
 
+/// Token can represent either a `Number` or an `Operator`
+///
+/// Now, one can create a `Vec<Token>` with numbers and operators mixed without 
+/// losing type safety.
+///
+/// ```
+/// let a = Number(1);
+/// let b = Operator('+');
+/// let output = vec![a, b];
+/// ```
+///
 #[derive(Debug, Eq, PartialEq)]
 enum Token {
     Number(u64),
@@ -80,14 +94,44 @@ enum Token {
 }
 
 #[cfg(test)]
-mod tests {
+mod shunting_yard_tests {
     
     use super::*;
 
     #[test]
     fn single_digit_works() {
         let test_str = "1";
-        let res = shunting_yard(test_str);
+        let res = shunting_yard(test_str).unwrap();
         assert_eq!(res[0], Number(1));
+    }
+
+    #[test]
+    fn one_plus_one_works() {
+        let test_str = "1+1";
+        let res = shunting_yard(test_str).unwrap();
+        let correct = vec![Number(1), Number(1), Operator('+')];
+        assert_eq!(res, correct);
+    }
+
+    #[test]
+    fn one_plus_two_times_four_works() {
+        let test_str = "1 + 2 * 4";
+        let res = shunting_yard(test_str).unwrap();
+        let correct = vec![Number(1), Number(2), Number(4), Operator('*'), Operator('+')];
+        assert_eq!(res, correct);
+    }
+
+    #[test]
+    fn mismatched_right_parenthesis_errors() {
+        let test_str = "1 + 2 * 3)";
+        let res = shunting_yard(test_str);
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn mismatched_left_parenthesis_errors() {
+        let test_str = "1 + (2 * 3";
+        let res = shunting_yard(test_str);
+        assert!(res.is_err());
     }
 }
