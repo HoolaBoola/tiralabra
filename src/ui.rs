5c29ac2a138f5a4c7947fd2091a1982b77960fbe
HoolaBoola <ale@hoolaboola.fi>
2022-02-12 19:30:59 +0200

    Testausdokumentti

--- src/ui.rs
+++ src/ui.rs
@@ -1,7 +1,7 @@
 use crate::logic::Calculator;
 use std::io::{stdin, stdout, Write};
 
-pub fn main_loop() -> Result<(), Box<dyn std::error::Error>> {
+pub fn main_loop() -> Result<(), std::io::Error> {
     let mut calculator = Calculator::new();
 
     let control_key = "?";
